2e9a4847752e9c711bc5a31cd4c69133b77d751a
WtsCrackin <galomeggiolarobul@gmail.com>
2022-04-23 22:12:50 -0300

    db

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,8 +4,71 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    img: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    area: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    currency: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    }
+  });
+  sequelize.define('activity', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dificulty: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5
+      }
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    season: {
+      type: DataTypes.ENUM("summer", "winter", "autumn", "spring"),
+      allowNull: true,
+    },
+    country: {
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
   });
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,75 @@
 const { Router } = require('express');
+const { Op } = require('sequelize');
+const { Country } = require('../db.js');
+const axios = require('axios').default;
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
+router.get("/countries/:id", async (req, res, next) => {
+    const { id } = req.params
+    console.log(id)
+    try {
+        const country = await Country.findAll({
+            where: {
+                id
+            }
+        })
+        console.log(country)
+        res.json(country)
+    } catch (error) {
+        next(error)
+    }
+})
 
+router.get("/countries", async (req, res, next) => {
+    const { name } = req.query
+    try {
+        let AllC = await Country.findAll()
+        if (AllC.length === 0) {
+            const response = await axios.get('https://restcountries.com/v3/all')
+            const count = response.data.filter(e => e.capital?.length > 0 && e.currencies)
+            count.forEach((e) => {
+                Country.create({
+                    id: e.cca3.toLowerCase(),
+                    name: e.name.common.toLowerCase(),
+                    img: e.flags[0],
+                    continent: e.region.toLowerCase(),
+                    capital: e.capital[0].toLowerCase(),
+                    subregion: e.subregion || null,
+                    population: e.population,
+                    area: e.area,
+                })
+            })
+            AllC = await Country.findAll()
+            res.json(AllC)
+            return
+        }
+        if (name) {
+            const countries = await Country.findAll({
+                where: {
+                    name: {
+                        [Op.like]: `%${name.toLowerCase()}%`
+                    }
+                }
+            })
+            console.log(countries)
+            res.json(countries.length > 0 ? countries : "no hay paises con ese nombre")
+            return
+        }
+        res.json(AllC)
+
+    }
+    catch (error) {
+        next(error)
+    }
+})
+
+//GET /countries
+//get /countries(idpais)
+// get countries?name
+//post activity
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
