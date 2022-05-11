6eec13b1a5b98cb81f8b25097f97705ccae1b191
WtsCrackin <galomeggiolarobul@gmail.com>
2022-05-11 15:13:52 -0300

    bug fixed

--- client/src/content/cards/CardMap.js
+++ client/src/content/cards/CardMap.js
@@ -4,7 +4,15 @@ import ActivityCard from './ActivityCard'
 import Card from './Card'
 
 const CardMap = ({ countries, contentType }) => {
-    return (contentType === "countries" ? countries.map((e) => <Card {...e} key={e.id} />) : countries.length > 0 ? countries.map((e) => <ActivityCard {...e} key={e.id} />) : <div className='noActivities__flex'><h1 className='noActivities'>THERE ARE NO ACTIVITIES</h1> <Link to="/home/form" className='noActivities-CreateOne'>Create one</Link></div>)
+    return (
+        contentType === "countries" ?
+            countries.length > 0 ?
+                countries.map((e) => <Card {...e} key={e.id} />) :
+                <div className='noActivities__flex'><h1 className='noActivities'>THERE ARE NO COUNTRIES WITH THAT NAME</h1></div>
+            :
+            countries.length > 0
+                ? countries.map((e) => <ActivityCard {...e} key={e.id} />)
+                : <div className='noActivities__flex'><h1 className='noActivities'>THERE ARE NO ACTIVITIES</h1> <Link to="/home/form" className='noActivities-CreateOne'>Create one</Link></div>)
 
 }
 
